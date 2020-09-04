a283ef6438ea5f0bf910d977e4c3479d6f2ef36c
ushibutatory <ushibutatory@gmail.com>
2020-09-04 15:26:31 +0900

    refactor: replace to `yield return`.

--- dotnet/src/AtCoderSolution/AtCoder.ABC107.D/Calculator.cs
+++ dotnet/src/AtCoderSolution/AtCoder.ABC107.D/Calculator.cs
@@ -29,15 +29,13 @@ namespace AtCoder.ABC107.D
 
         public IEnumerable<IEnumerable<long>> GetSubLists(IEnumerable<long> list)
         {
-            var subLists = new List<IEnumerable<long>>();
             for (var i = 0; i < list.Count(); i++)
             {
                 for (var j = i; j < list.Count(); j++)
                 {
-                    subLists.Add(list.Skip(i).Take(j - i + 1));
+                    yield return list.Skip(i).Take(j - i + 1);
                 }
             }
-            return subLists;
         }
     }
 }
